3968ba295fabec91dbd66fa75aaba1292ab0fdbc
TranBinh2311 <binhlinh473@gmail.com>
2021-12-28 17:57:59 +0700

    Siuu

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -127,6 +127,9 @@ function handlePostback(sender_psid, received_postback) {
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
+  else if(payload === 'GET_STARTED') {
+    response = { "text":"Chao mung ban den voi trang cua toi" }
+  }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
