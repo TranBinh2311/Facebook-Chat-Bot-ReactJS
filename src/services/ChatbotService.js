a1e7b4f8e35e15b5b670e531ccfa74ce4a9cc711
TranBinh2311 <binhlinh473@gmail.com>
2021-12-28 19:37:42 +0700

    update

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -31,14 +31,13 @@ let getUsername = (sender_psid) => {
       return new Promise((resolve, reject)=>{
         request({
             "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-            "qs": { "access_token": PAGE_ACCESS_TOKEN  },
-            "method": "POST",
-            "json": request_body
+            "method": "GET",
           }, (err, res, body) => {
             if (!err) {
                 // "first_name": "Peter",
                 // "last_name": "Chang",
                 body = JSON.parse(body);
+                console.log(body);
                 let username = `${body.first_name} ${body.last_name}`;
                 resolve(username);
             } else {
